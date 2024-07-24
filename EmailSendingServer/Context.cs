897cfc1e901113c6719c9f0c0aa3dc40c74ad9e1
Mishleyn <svalov1985@bk.ru>
2024-07-24 15:42:26 +0700

    Добавьте файлы проекта.

---
+++ EmailSendingServer/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserLogin.Models;
+
+
+namespace EmailSendingServer;
+
+public class Context : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=HONORBOOK\\SQLEXPRESS;Database=razorpagesdb;Trusted_Connection=True;TrustServerCertificate=True");
+    }
+}
---
+++ EmailSendingServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using EmailSendingServer;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        var context = new Context();
+        var lastId = context.Users.OrderByDescending(u => u.Id).FirstOrDefault()?.Id ?? 0;
+
+        while (true)
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var newUsers = context.Users.Where(u => u.Id > lastId).ToList();
+
+                    if (newUsers.Any())
+                    {
+                        foreach(var user in newUsers)
+                        {
+                            Console.WriteLine($"New user: Id: {user.Id} Email: {user.Email} " +
+                                $"Confirmation Code: {user.ConfirmationCode}");
+                        }
+
+                        lastId = newUsers.Last().Id;
+                    }
+
+                    transaction.Commit();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Errors: {ex.Message}");
+                    transaction.Rollback();
+                }
+            }
+            Task.Delay(5000).Wait();
+        }
+    }
+}
